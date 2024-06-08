cccd2eff8cc3a360dcfb2765f5a9e4e9edaf8ac7
沉默の金 <110812055+chenmozhijin@users.noreply.github.com>
2024-06-09 01:34:13 +0800

    Workflow supports "-beta" in the version

--- setup.py
+++ setup.py
@@ -6,13 +6,15 @@ import LDDC
 parser = argparse.ArgumentParser()
 parser.add_argument('--task', choices=['get_version', 'get_year'], required=True)
 arg = parser.parse_args()
-version = LDDC.__version__.replace('v', '')
+
+version = ".".join([i.split("-")[0] for i in LDDC.__version__.replace("v", "").split(".")])
+
 year = time.strftime("%Y")
 if year != '2024':
     year = "2024-" + year
 
 match arg.task:
     case 'get_version':
-        print(version)
+        print()
     case 'get_year':
         print(year)
