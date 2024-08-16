43354b658d113d47791ba00d20156e042ebaa5bb
沉默の金 <110812055+chenmozhijin@users.noreply.github.com>
2024-08-17 01:54:59 +0800

    fix: Wrong build workflow

--- build_helper.py
+++ build_helper.py
@@ -1,13 +1,15 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024 沉默の金 <cmzj@cmzj.org>
+# SPDX-License-Identifier: GPL-3.0-only
 import argparse
 import time
 
-import LDDC
+from utils.version import __version__, parse_version
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--task', choices=['get_version', 'get_year'], required=True)
 arg = parser.parse_args()
 
-version = ".".join([i.split("-")[0] for i in LDDC.__version__.replace("v", "").split(".")])
+version = ".".join(str(i) for i in parse_version(__version__)[:3])
 
 year = time.strftime("%Y")
 if year != '2024':
