8cbe737f10bfcc3b350ce27837b9b4125dfcdcda
沉默の金 <110812055+chenmozhijin@users.noreply.github.com>
2024-10-01 15:53:59 +0800

    feat: Optimize code

--- LDDC/ui/update_ui.py
+++ LDDC/ui/update_ui.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 
 ################################################################################
 ## Form generated from reading UI file 'update.ui'
@@ -8,41 +7,34 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QAbstractButton, QApplication, QDialog, QDialogButtonBox,
-    QLabel, QSizePolicy, QTextBrowser, QVBoxLayout,
-    QWidget)
-import res.resource_rc
+from PySide6.QtCore import QCoreApplication, QMetaObject, QSize, Qt
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QDialogButtonBox, QLabel, QTextBrowser, QVBoxLayout
 
-class Ui_UpdateDialog(object):
+
+class Ui_UpdateDialog:
     def setupUi(self, UpdateDialog):
         if not UpdateDialog.objectName():
-            UpdateDialog.setObjectName(u"UpdateDialog")
+            UpdateDialog.setObjectName("UpdateDialog")
         UpdateDialog.resize(538, 322)
         icon = QIcon()
-        icon.addFile(u":/LDDC/img/icon/logo.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        icon.addFile(":/LDDC/img/icon/logo.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
         UpdateDialog.setWindowIcon(icon)
         self.verticalLayout = QVBoxLayout(UpdateDialog)
-        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setObjectName("verticalLayout")
         self.label = QLabel(UpdateDialog)
-        self.label.setObjectName(u"label")
+        self.label.setObjectName("label")
 
         self.verticalLayout.addWidget(self.label)
 
         self.textBrowser = QTextBrowser(UpdateDialog)
-        self.textBrowser.setObjectName(u"textBrowser")
+        self.textBrowser.setObjectName("textBrowser")
         self.textBrowser.setOpenExternalLinks(True)
 
         self.verticalLayout.addWidget(self.textBrowser)
 
         self.buttonBox = QDialogButtonBox(UpdateDialog)
-        self.buttonBox.setObjectName(u"buttonBox")
+        self.buttonBox.setObjectName("buttonBox")
         self.buttonBox.setOrientation(Qt.Orientation.Horizontal)
         self.buttonBox.setStandardButtons(QDialogButtonBox.StandardButton.No|QDialogButtonBox.StandardButton.Yes)
 
@@ -57,8 +49,8 @@ class Ui_UpdateDialog(object):
     # setupUi
 
     def retranslateUi(self, UpdateDialog):
-        UpdateDialog.setWindowTitle(QCoreApplication.translate("UpdateDialog", u"\u53d1\u73b0\u65b0\u7248\u672c", None))
-        self.label.setText(QCoreApplication.translate("UpdateDialog", u"\u53d1\u73b0\u65b0\u7248\u672c\uff0c\u662f\u5426\u524d\u5f80GitHub\u4e0b\u8f7d\u66f4\u65b0\uff1f", None))
+        UpdateDialog.setWindowTitle(QCoreApplication.translate("UpdateDialog", "\u53d1\u73b0\u65b0\u7248\u672c", None))
+        self.label.setText(QCoreApplication.translate("UpdateDialog", "\u53d1\u73b0\u65b0\u7248\u672c\uff0c\u662f\u5426\u524d\u5f80GitHub\u4e0b\u8f7d\u66f4\u65b0\uff1f", None))
         self.textBrowser.setMarkdown("")
     # retranslateUi
 
