ee81ae3745b777612bf4c5c9cba5a29e715074ca
沉默の金 <110812055+chenmozhijin@users.noreply.github.com>
2025-06-06 22:51:46 +0800

    fix(auto_fetch): TypeError: reduce() of empty iterable with no initial value

--- LDDC/core/auto_fetch.py
+++ LDDC/core/auto_fetch.py
@@ -239,9 +239,14 @@ def auto_fetch(
                     if not return_search_results
                     else (
                         lyrics,
-                        (search_results[info] + reduce(lambda a, b: a + b, [result for song_info, result in search_results.items() if song_info != info])),
+                        (
+                            (search_results[info] + reduce(lambda a, b: a + b, other_search_results))
+                            if (other_search_results := [result for song_info, result in search_results.items() if song_info != info])
+                            else search_results[info]
+                        ),
                     )
                 )
+
         continue
     msg = "自动获取时发生未知错误"
     raise AutoFetchUnknownError(msg, errors)
